20696f88f9051dd48185000ef8efd5b78218d1aa
Yurifrl <yurifrl@hotmail.com>
2014-07-07 14:32:54 -0300

    update

--- lib/shipping_watcher/calculator.rb
+++ lib/shipping_watcher/calculator.rb
@@ -1,4 +1,5 @@
 require 'correios-frete'
+
 class Calculator
   def self.compute()
     frete    = Correios::Frete::Calculador.new(:cep_origem => "04094-050", :cep_destino => "90619-900", :peso => 0.3, :comprimento => 30, :largura => 15, :altura => 2)
