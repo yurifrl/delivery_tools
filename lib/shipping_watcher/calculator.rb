422af94c87defeea23b09c0a14238e64db5345e5
Yurifrl <yurifrl@hotmail.com>
2014-07-07 15:28:06 -0300

    update

--- lib/shipping_watcher/calculator.rb
+++ lib/shipping_watcher/calculator.rb
@@ -7,17 +7,9 @@ class Calculator
   @comprimento
   @largura
   @altura
-  def initialize(cep_origem:, cep_destino:, peso:, comprimento:, largura:, altura:)
-    @cep_origem  = cep_origem
-    @cep_destino = cep_destino
-    @peso        = peso
-    @comprimento = comprimento
-    @largura     = largura
-    @altura      = altura
-  end
 
   def compute()
     frete = Correios::Frete::Calculador.new(:cep_origem => @cep_origem, :cep_destino => @cep_destino, :peso => @peso, :comprimento => @comprimento, :largura => @largura, :altura => @altura)
-    frete.calcular(:sedex, :pac)
+    frete.calcular(:sedex, :pac, :sedex_10)
   end
 end
