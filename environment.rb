98f0645dc42189ecaf58ce6781fdda93e432d3ff
Yurifrl <yurifrl@hotmail.com>
2014-07-08 09:58:17 -0300

    teste

--- environment.rb
+++ environment.rb
@@ -9,15 +9,17 @@ module Application
   include ActiveSupport::Configurable
 end
 
+
 Application.configure do |config|
   config.root     = File.dirname(__FILE__)
   config.env      = ActiveSupport::StringInquirer.new(env.to_s)
 end
 
-begin
+
 db_config = YAML.load(ERB.new(File.read('config/database.yml')).result)[Application.config.env]
 ActiveRecord::Base.default_timezone = :utc
 ActiveRecord::Base.establish_connection(db_config)
-rescue
-  p 'hello'
-end
+
+specific_environment = "config/environments/#{Application.config.env}.rb"
+require specific_environment if File.exists? specific_environment
+Dir["config/initializers/**/*.rb"].each {|f| require f}
