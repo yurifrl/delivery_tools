e3d2b973cae4f0e3a7c6cff74beb6847b63ff515
yurifrl <yurifrl@hotmail.com>
2015-01-09 16:30:18 -0200

    remove hardcode

--- lib/shipping_watcher/notifier.rb
+++ lib/shipping_watcher/notifier.rb
@@ -6,7 +6,6 @@ class Notifier
   format :json
 
   def self.request(tracker)
-    tracker.url = "http://store1.lvh.me:3000/api/order_tracker"
     base_uri(tracker.url)
     post("#{tracker.url}?token=#{tracker.api_key}", :body => {tracking_code: tracker.code, status_name: tracker.status.name, status_id: tracker.status_id})
   end
